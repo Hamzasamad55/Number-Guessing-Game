0c17361e9eec0c2d8b313deb3adb7b8ba68c5305
Hamza Samad <160117284+Hamzasamad55@users.noreply.github.com>
2024-05-08 01:00:17 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+// computer will generate a random number.
+
+// user input for guessing number
+
+//  computer user input with computer generated number and show result
+
+const randomNumber = Math.floor(Math.random() * 6+1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number between 1 to 5",
+  },
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congratulations! You Guessed Right Number")
+}else {
+    console.log("Sorry! You Guessed Wrong Number");
+}
